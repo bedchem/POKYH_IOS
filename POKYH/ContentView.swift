502c74e4aa38387a1f5d4ee8e157c0d159281077
Nexor <felix.plattner312009@outlook.de>
2026-06-11 21:11:57 +0200

    Initial Commit

---
+++ POKYH/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POKYH
+//
+//  Created by Felix Plattner on 6/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POKYH/POKYHApp.swift
@@ -0,0 +1,17 @@
+//
+//  POKYHApp.swift
+//  POKYH
+//
+//  Created by Felix Plattner on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct POKYHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
